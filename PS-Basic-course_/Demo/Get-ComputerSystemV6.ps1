8996a8915b13b53239b0d225c57dad8b99319380
yusisushi <yusisushi@gmail.com>
2022-09-05 15:50:04 +0200

    .\PS-Basic-course_\

---
+++ PS-Basic-course_/Demo/Get-ComputerSystemV6.ps1
@@ -0,0 +1,40 @@
+﻿<#
+	.SYNOPSIS
+	The sort description of the function.
+	
+	.DESCRIPTION
+	The long description of the function.
+
+    .PARAMETER computername
+    The name of the computer we are examining
+
+    .PARAMETER detail
+    Switch that indicates if detailed information is needed
+	
+	.EXAMPLE
+	Get-ComputerSystem -ComputerName DC01 -Detail
+#>
+
+[CmdletBinding()]
+param(
+	$ComputerName="localhost",
+	[switch]$Detail
+)
+
+Write-Verbose "Connecting to computer $ComputerName"
+
+if ($Detail) {
+	Get-WmiObject -Class Win32_ComputerSystem -ComputerName $ComputerName | 
+    Select-Object Name, Model, Manufacturer, SystemType, Domain, BootUpState, 
+    @{name="Memory (GB)";expression={$_.TotalPhysicalMemory/1GB -as [int]}}
+}
+else {
+	Get-WmiObject -Class Win32_ComputerSystem -ComputerName $ComputerName | 
+    Select-Object Name, Model, Manufacturer
+}
+
+write-verbose "End for $ComputerName"
+
+<# NEXT STEP
+We prefer that the script asks for a computername.
+#>
